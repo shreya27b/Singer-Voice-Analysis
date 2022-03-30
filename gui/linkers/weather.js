0c5874bef099137ce8483a0b30e78696a9d234c3
shreya27b <shreyabarhate2468@gmail.com>
2022-03-30 12:16:16 +0530

    Later Commit

--- gui/linkers/weather.js
+++ gui/linkers/weather.js
@@ -1,21 +1,21 @@
-let {PythonShell} = require('python-shell')
+let { PythonShell } = require('python-shell')
 var path = require("path")
 
 
 function get_weather() {
 
-  var city = document.getElementById("city").value
-  
-  var options = {
-    scriptPath : path.join(__dirname, '/../engine/'),
-    args : [city]
-  }
+    var city = document.getElementById("city").innerHTML
 
-  let pyshell = new PythonShell('weather_engine.py', options);
+    var options = {
+        scriptPath: path.join(__dirname, '/../engine/'),
+        args: [city]
+    }
+
+    let pyshell = new PythonShell('weather_engine.py', options);
 
 
-  pyshell.on('message', function(message) {
-    swal(message);
-  })
-  document.getElementById("city").value = "";
+    pyshell.on('message', function(message) {
+        swal(message);
+    })
+    document.getElementById("city").value = "";
 }
